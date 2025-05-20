72d316cd25bbf56e06875de1597e16fdee8e598c
arnav miranda <arnavmiranda@gmail.com>
2025-05-20 16:36:22 +0400

    done with LLDQ except for i)iterables & ii)equals

--- proj1/deque/LList.java
+++ proj1/deque/LList.java
@@ -1,18 +1,6 @@
 package deque;
 
-
-
-class LList<T> {
-
-    /**
-     * Internal private class for the foundation of a Singly Linked List, ie SLL.
-     * This Node class is not accessed by any other class outside SSL, and is
-     * hence kept private. If need be, can be changed to public without much issue.
-     * <p>
-     * Ultimately this LList is implemented through a Circular Sentinel Node Doubly
-     * Linked List data structure.
-     */
-
+class LinkedListDeque<T> {
     private class Node {
         public T item;
         public Node next;
@@ -25,72 +13,51 @@ class LList<T> {
         }
     }
 
-    /* Sentinel Node implementation of Singly Linked Lists. */
     private Node sentinel = new Node(null, null, null);
     private int size;
 
-    public LList() {
+    public LinkedListDeque() {
         size = 0;
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
     }
-
-    public LList(T x) {
+    public LinkedListDeque(T x) {
         Node element = new Node(x, sentinel, sentinel);
         sentinel.next = element;
         sentinel.prev = element;
         size = 1;
     }
 
-    /**
-     * Adds element to beginning of LL.
-     */
     public void addFirst(T x) {
         Node element = new Node(x, sentinel.next, sentinel);
         sentinel.next.prev = element;
         sentinel.next = element;
         size += 1;
     }
-
-    /**
-     * Returns first element of LL.
-     */
     public T getFirst() {
         return sentinel.next.item;
     }
-
-    /**
-     * Adds a node to the final node of SLL.
-     */
     public void addLast(T x) {
         Node element = new Node(x, sentinel, sentinel.prev);
         sentinel.prev.next = element;
         sentinel.prev = element;
         size += 1;
     }
-
     public T getLast() {
         return sentinel.prev.item;
     }
-
-    /**
-     * Returns size of the SLL, calculated after the sentinel node.
-     */
     public int size() {
         return this.size;
     }
-
     public boolean isEmpty() {
         if (sentinel.next == null && sentinel.prev == null)
             return true;
         return false;
     }
-
     public void makeEmpty() {
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
     }
-
     public void printDeque() {
         if (!isEmpty()) {
             Node iter = sentinel.next;
@@ -101,7 +68,6 @@ class LList<T> {
             System.out.println();
         }
     }
-
     public T removeFirst() {
         if (isEmpty()) {
             return null;
@@ -112,7 +78,6 @@ class LList<T> {
         size--;
         return element.item;
     }
-
     public T removeLast() {
         if (isEmpty()) {
             return null;
@@ -124,20 +89,24 @@ class LList<T> {
         return element.item;
     }
     public T get(int index) {
-        int c = index;
-        Node recur = sentinel.next;
-        while(recur.next != sentinel) {
-            if(c == 0) {
-                return recur.item;
+        Node iter = sentinel.next;
+        for(int i = 0; i < size; i++) {
+            if(i == index) {
+                return iter.item;
             }
-            recur = recur.next;
-            c++;
+            iter = iter.next;
         }
         return null;
     }
-}
-
-
-/*TODO: dont forget to specify that if  sentinel is empty- both next and priv must be NULL */
-public class LinkedListDeque {
+    public T getRecursive(int index) {
+        Node recur = sentinel.next;
+        while(recur.next != sentinel) {
+            if(index == 0) {
+                return recur.item;
+            }
+            recur = recur.next;
+            index++;
+        }
+        return null;
+    }
 }
