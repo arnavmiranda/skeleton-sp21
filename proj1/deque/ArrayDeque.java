b3c63bf2b550e6728290790f7f9ca64613d0b37d
arnav miranda <arnavmiranda@gmail.com>
2025-05-21 21:33:24 +0400

    rising from the ashes isnt so easy

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,185 +1,177 @@
 package deque;
 
 public class ArrayDeque<T> {
-    private T[] items;
-    private int size;
-    public static final int INITIAL_ARRAY_SIZE = 8;
-    public static final int R_FACTOR = 2;
 
-    private int nextFirst;
-    private int nextLast;
+    public static final int ARRAY_SIZE = 8;
+    public static final int RESIZE_FACTOR = 2;
+    private T[] items = (T[]) new Object[ARRAY_SIZE];
+
+    private int nextStart;
+    private int nextEnd;
+    private int size;
 
     public ArrayDeque() {
-        items = (T[]) new Object[INITIAL_ARRAY_SIZE];
+        nextStart = 3;
+        nextEnd = 4;
         size = 0;
-        nextFirst = 3;
-        nextLast = 4;
     }
-    private int first(){
-        if(!isEmpty()){
-            if(nextFirst != (items.length - 1)) {
-                return nextFirst + 1;
-            } else {
-                return 0;
-            }
+
+    private int length() {
+        return items.length;
+    }
+
+    private int start() {
+        if (nextStart == items.length) {
+            return 0;
         }
-        return -1;
+        return nextStart + 1;
     }
-    private int last(){
-        if(!isEmpty()){
-            if(nextLast != 0) {
-                return nextLast - 1;
-            } else {
-                return items.length - 1;
-            }
+
+    private int end() {
+        if (nextEnd == 0) {
+            return items.length;
         }
-        return -1;
+        return nextEnd - 1;
     }
-    private boolean needsReducing(){
-        double USAGE_RATIO = (double) size / items.length;
-        return (USAGE_RATIO < 0.25);
-    }
-    private boolean needsResizing() {
+
+    private boolean full() {
         return (size == items.length);
     }
-    private void reduceArray() {
-        if(items.length>16) {
-            while(needsReducing()) {
-                resize(items.length / 2);
+
+    private boolean startFromFirst() {
+        return (start() == 0);
+    }
+
+    private boolean endsAtLast() {
+        return (end() == items.length - 1);
+    }
+
+    private void compress() {
+        double USAGE_RATIO = (double) size / length();
+        while (USAGE_RATIO < 0.25) {
+            T[] temp = (T[]) new Object[length() / 2];
+            int start = start();
+            if (startFromFirst()) {
+                System.arraycopy(items, 0, temp, 0, size);
+            } else {
+                System.arraycopy(items, start, temp, 0, length() - start);
+                System.arraycopy(items, 0, temp, length() - start + 1, start);
             }
+            items = temp;
+            nextStart = length() - 1;
+            nextEnd = size;
+            USAGE_RATIO = (double) size / length();
         }
     }
-    private void nextFirst() {
-        if(nextFirst != 0) {
-            nextFirst--;
+
+    private void resize(int capacity) {
+        if (!full()) return;
+
+        T[] temp = (T[]) new Object[capacity];
+        int start = start();
+        if (startFromFirst()) {
+            System.arraycopy(items, 0, temp, 0, size);
         } else {
-            nextFirst = items.length - 1;
+            System.arraycopy(items, start, temp, 0, length() - start);
+            System.arraycopy(items, 0, temp, length() - start + 1, start);
         }
-        if (needsResizing()) {
-            resize(size * R_FACTOR);
+        items = temp;
+        nextStart = length() - 1;
+        nextEnd = size;
+    }
+
+    private void changeNextStart() {
+        if (!full()) {
+            if (nextStart == 0) {
+                nextStart = length() - 1;
+                return;
+            }
+            nextStart--;
         }
     }
-    private void nextLast() {
-        if (nextLast != (items.length - 1)) {
-            nextLast++;
-        } else {
-            nextLast = 0;
-        }
-        if (!needsResizing()) {
-            resize(size * R_FACTOR);
+
+    private void changeNextEnd() {
+        if (!full()) {
+            if (nextEnd == length() - 1) {
+                nextEnd = 0;
+                return;
+            }
+            nextEnd++;
         }
     }
-     private void resize(int capacity) {
-     T[] temp = (T[]) new Object[capacity];
-     if(capacity > items.length) {
-         if(nextFirst == (size - 1) && nextLast == 0) {
-             System.arraycopy(items, 0, temp, 0, size);
-             nextFirst = temp.length - 1;
-             nextLast = size;
-         } else {
-             int first = nextFirst + 1;
-             System.arraycopy(items, first, temp, 0, size - first);
-             System.arraycopy(items, 0, temp, size - first, first);
-             nextFirst = capacity - 1;
-             nextLast = size;
-         }
-     }
-     if(capacity < items.length) {
-         int first = first();
-         int last = last();
-         if(first < last) {
-             System.arraycopy(items, first, temp, 0, size);
-             nextFirst = capacity - 1;
-             nextLast = size;
-         }
-         if (first > last) {
-             int index = 0;
-             for(int i = first; i < size; i++) {
-                 temp[index] = items[i];
-                 index++;
-             }
-             for(int i = 0; i < last + 1; i++) {
-                 temp[index] = items[i];
-                 index++;
-             }
-             nextFirst = capacity - 1;
-             nextLast = size;
-         }
-     }
-     items = temp;
-     }
+
     public void addFirst(T item) {
-        if (size == items.length) {
-            size++;
-            nextFirst();
-            items[nextFirst] = item;
-        } else {
-            items[nextFirst] = item;
-            nextFirst();
-            size++;
-        }
-    }
-    public void addLast(T item) {
-        items[nextLast] = item;
+        resize(size * RESIZE_FACTOR);
+        items[nextStart] = item;
         size++;
-        nextLast();
+        changeNextStart();
     }
-    public T get(int index) {
-        if(isEmpty()) {
-            return null;
-        }
-        if(index > size) {
-            return null;
-        }
-        int first = first();
-        int last = last();
-        if (first < last) {
-            return items[first + index];
-        }
-        if(index < size - first) {
-            return items[first + index];
-        }
-        return items[index + first - size];
+
+    public void addLast(T item) {
+        resize(size * RESIZE_FACTOR);
+        items[nextEnd] = item;
+        size++;
+        changeNextEnd();
     }
-    public T removeLast() {
-        if (isEmpty()) {
-            return null;
-        }
-        int last = last();
-        T item = items[last];
-        items[last] = null;
-        nextLast = last;
-        size--;
-        reduceArray();
-        return item;
-    }
-    public T removeFirst() {
-        if (isEmpty()) {
-            return null;
-        }
-        int first = first();
-        T item = items[first];
-        items[first] = null;
-        nextFirst = first;
-        size--;
-        reduceArray();
-        return item;
-    }
+
     public int size() {
         return size;
     }
+
     public boolean isEmpty() {
         return (size == 0);
     }
+
+    public T get(int index) {
+        int start = start();
+        int last = items.length - 1;
+
+        if (isEmpty()) {
+            return null;
+        }
+        if (index > size) {
+            return null;
+        }
+        if (start + index > last) {
+            return items[start + index - last];
+        }
+        return items[start + index];
+    }
+
+    public T removeFirst() {
+        T item = items[start()];
+        items[start()] = null;
+        size--;
+        nextStart = start();
+        compress();
+        return item;
+    }
+
+    public T removeLast() {
+        T item = items[end()];
+        items[end()] = null;
+        size--;
+        nextEnd = end();
+        compress();
+        return item;
+    }
+
     public void printDeque() {
-        int first = first();
-        int last = last();
-        while(first != last + 1) {
-            System.out.print(items[first] + " ");
-            first++;
-            if(first == items.length){
-                first = 0;
+        if (isEmpty()) {
+            return;
+        }
+        if (start() < end()) {
+            for (int i = start(); i < end(); i++) {
+                System.out.print(items[i] + " ");
+            }
+        } else {
+            for (int i = start(); i < length(); i++) {
+                System.out.print(items[i] + " ");
+            }
+            for (int i = 0; i <= end(); i++) {
+                System.out.print(items[i] + " ");
             }
         }
+        System.out.println();
     }
 }
