d7cb2cb7b3b48dd8f71c5ae7b7ed972a1bf2f54e
arnav miranda <arnavmiranda@gmail.com>
2025-05-23 12:44:37 +0400

    plays note A

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -9,9 +9,8 @@ public class GuitarString {
      * other topics in lecture on Friday. */
     private static final int SR = 44100;      // Sampling Rate
     private static final double DECAY = .996; // energy decay factor
-
+    private double store;
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
     private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
@@ -21,39 +20,36 @@ public class GuitarString {
         double dCapacity = SR / frequency;
         int capacity = (int) Math.round(dCapacity);
 
-        for(int i = 0; i<= )
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
+        for(int i = 0; i< capacity; i++) {
+            buffer.addLast(0.0);
+        }
     }
-
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each
-        //       other. This does not mean that you need to check that the numbers
-        //       are different from each other. It means you should repeatedly call
-        //       Math.random() - 0.5 to generate new random numbers for each array index.
+        int capacity = buffer.size();
+        while (!buffer.isEmpty()) {
+            buffer.removeFirst();
+        }
+        for (int i = 0; i < capacity; i++) {
+            double r = Math.random() - 0.5;
+            buffer.addFirst(r);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        double first = buffer.removeFirst();
+        double second = buffer.get(0);
+        double average = (first + second) / 2;
+        double newDouble = average * DECAY;
+        buffer.addLast(newDouble);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+    return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
